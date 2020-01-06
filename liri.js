46ec501d4751a54254b75ce19f29b4bab79f273c
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-06 05:02:49 -0500

    fixed keys

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
-require('dotenv').config()
+require("dotenv").config()
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require("moment")
 var fs = require('fs');
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
 
 console.log(process.env.SPOTIFY_ID) 
