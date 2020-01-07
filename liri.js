15f729c9daf9bc739f8117c377f458030ddc56c9
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-06 21:15:44 -0500

    kes and random folder created and tested - working

--- liri.js
+++ liri.js
@@ -3,9 +3,48 @@ var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var moment = require("moment")
 var fs = require('fs');
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-});
+var keys = require("./keys.js")
 
-console.log(process.env.SPOTIFY_ID) 
+// var spotify = new Spotify({
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+// });
+var spotify = new Spotify(keys.spotify)
+
+// console.log(process.env.SPOTIFY_ID) 
+
+//Takes in argument which will be an action telling liri what to do
+var action = process.argv[2];
+// console.log(action);
+//Takes in argument which will be user input
+var input = process.argv.slice(3).join(" ");
+// console.log(input);
+
+
+//Creating a switch-case statement. This will direct which function gets to run.
+function command (userInput, userQuery){
+
+	switch (userInput){
+
+        case"concert-this":
+            concertThis();
+            break;
+
+		case "spotify-this-song":
+			spotifyThisSong();
+			break;
+
+		case "movie-this":
+			movieThis();
+			break;
+
+		case "do-what-it-says":
+			doWhatItSays();
+            break;
+
+        default:
+			console.log("I do not understand!");
+			break;
+	}
+}
+command(userInput, userQuery);
