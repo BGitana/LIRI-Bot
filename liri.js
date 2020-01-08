bcdbd72db4c009bf2e1f39fd3185b9eef1c4abd2
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-08 05:08:48 -0500

    function spotify-this-song is working

--- liri.js
+++ liri.js
@@ -16,9 +16,11 @@ var spotify = new Spotify(keys.spotify)
 //Takes in argument which will be an action telling liri what to do
 var userInput = process.argv[2];
 // console.log(action);
+// console.log(userInput);
 //Takes in argument which will be user input
 var userQuery = process.argv.slice(3).join(" ");
 // console.log(input);
+// console.log(userQuery);
 
 
 //Creating a switch-case statement. This will direct which function gets to run.
@@ -51,70 +53,65 @@ command(userInput, userQuery);
 
 
 
+// function concertThis() {
+//     console.log(`\n------\n\nSearching for...${userQuery}'s next show...`);
+
+//     // USER REQUEST AS QUERY URL BY USING userQuery VARIABLE PARAMETER FOR OUR SEARCH
+//     request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"); {
+//         // IF THERE IS NO ERROR GIVE A 200 STATUS CODE (everything OK!)
+//         if (!error && response.statusCode === 200) {
+//             // CAPTURE DATA AND USE IT IN JSON FORMAT
+//             let userBand = JSON.parse(body);
+//             // USE FOR LOOP TO ACCESS PATH TO NEEDED DATA
+//             if(userBand.length > 0) { 
+//                 for ( i = 0; i < 1; i ++) {
+//                     // CONSOLE LOG NEEDED DATA
+//                     console.log(`\nThat's for you...\n\n
+//                     Artist: ${userBand[i].lineup[0]}\n
+//                     Venue: ${userBand[i].venue.name}\n
+//                     Venue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\n
+//                     Venue City/Country: ${userBand[i].venue.city},${userBand[i].venue.country}`)
+
+//                     // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
+//                     let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");  
+
+//                     console.log(`Date and Time: ${concertDate}\n\n--------`);
+//                     };
+//                 } else {
+//                     console.log(`Band or concert not found!`);
+//                 };
+//             }; 
+//         };
+//     };
 
 
-function concertThis() {
-    console.log(`\n------\n\nSearching for...${userQuery}'s next show...`);
+    function spotifyThisSong() {
+        console.log(`\n------\n\nSearching for..."${userQuery}"`);
 
-    // USE REQUEST AS QUERY URL BY USING userQuery VARIABLE PARAMETER FOR OUR SEARCH
-    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"); {
-        // IF THERE IS NO ERROR GIVE A 200 STATUS CODE (everything OK!)
-        if (!error && response.statusCode === 200) {
-            // CAPTURE DATA AND USE IT IN JSON FORMAT
-            let userBand = JSON.parse(body);
-            // USE FOR LOOP TO ACCESS PATH TO NEEDED DATA
-            if(userBand.length > 0) { 
-                for ( i = 0; i < 1; i ++) {
-                    // CONSOLE LOG NEEDED DATA
-                    console.log(`\nThat's for you...\n\n
-                    Artist: ${userBand[i].lineup[0]}\n
-                    Venue: ${userBand[i].venue.name}\n
-                    Venue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}} \n
-                    Venue City/Country: ${userBand[i].venue.city},${userBand[i].venue.country}`)
+        // IF userQuery NOT FOUND, PASS VALUE OF "The Sign" by Ace of Base
+        if (!userQuery) {userQuery= `"The Sign" by Ace of Base`}
 
-                    // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
-                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");  
+        // SPOTIFY EACH QUERY FORMAT
+        spotify.search({ type: 'track', query: `${userQuery}`, limit: 3 }, function(error, data) {
+            if(error){
+                return console.log(`Error occurred:`+ error);
+            }
+            // COLLECT SELECTED DATA IN AN ARRAY
+            let spotifyArr = data.tracks.items;
 
-                    console.log(`Date and Time: ${concertDate}\n\n--------`);
-                    };
-                } else {
-                    console.log(`Band or concert not found!`);
-                };
-            }; 
-        };
-    };
+            for (i= 0; i< spotifyArr.length; i++){
 
+                console.log(`\nThat's for you...\n\n
+                        Artist: ${data.tracks.items[i].album.artists[0].name}\n
+                        Song: ${data.tracks.items[i].name}\n
+                        Spotify Link: ${data.tracks.items[i].external_urls.spotify}\n
+                        Album: ${data.tracks.items[i].album.name}\n\n----------------`)
+            };
+        });
+    }
 
-        // axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp")
-        //     .then(function (response) {
-        //         console.log("");
-        //         var movieInfo;
-        //         for (var i = 0; i < 5; i++) {
-        //             movieInfo = ("\n" + (response.data[0].lineup[0]) + "\n" +
-        //                 (response.data[i].venue.name) + "\n" +
-        //                 (response.data[i].venue.city) + "," +
-        //                 (response.data[i].venue.region) + "\n" +
-        //                 (moment(response.data[i].datetime).format("MM/DD/YYYY")) + "\n");
-        //             console.log(movieInfo);
-        //             fs.appendFile("concert-log.txt", movieInfo, function read(err, data) {
-        //                 if (err) {
-        //                     throw err;
-        //                 }
-        //             });
-        //         }
-        //     })
-        //     .catch(function (error) {
-        //         if (error.response) {
-        //             console.log(error.response.data);
-        //             console.log(error.response.status);
-        //             console.log(error.response.headers);
-        //         } else if (error.request) {
-        //             console.log(error.request);
-        //         } else {
-        //             console.log("Error", error.message);
-        //         }
-        //         console.log(error.config);
-        //     });
+    
+        
 
     
 
