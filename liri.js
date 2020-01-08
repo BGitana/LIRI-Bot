a0142adb59c7b30f46c7296ab4bc31bcaafc4e15
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-08 03:32:29 -0500

    concert-this code finished

--- liri.js
+++ liri.js
@@ -48,3 +48,75 @@ function command (userInput, userQuery){
 	}
 }
 command(userInput, userQuery);
+
+
+
+
+
+function concertThis() {
+    console.log(`\n------\n\nSearching for...${userQuery}'s next show...`);
+
+    // USE REQUEST AS QUERY URL BY USING userQuery VARIABLE PARAMETER FOR OUR SEARCH
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"); {
+        // IF THERE IS NO ERROR GIVE A 200 STATUS CODE (everything OK!)
+        if (!error && response.statusCode === 200) {
+            // CAPTURE DATA AND USE IT IN JSON FORMAT
+            let userBand = JSON.parse(body);
+            // USE FOR LOOP TO ACCESS PATH TO NEEDED DATA
+            if(userBand.length > 0) { 
+                for ( i = 0; i < 1; i ++) {
+                    // CONSOLE LOG NEEDED DATA
+                    console.log(`\nThat's for you...\n\n
+                    Artist: ${userBand[i].lineup[0]}\n
+                    Venue: ${userBand[i].venue.name}\n
+                    Venue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}} \n
+                    Venue City/Country: ${userBand[i].venue.city},${userBand[i].venue.country}`)
+
+                    // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
+                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");  
+
+                    console.log(`Date and Time: ${concertDate}\n\n--------`);
+                    };
+                } else {
+                    console.log(`Band or concert not found!`);
+                };
+            }; 
+        };
+    };
+
+
+        // axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp")
+        //     .then(function (response) {
+        //         console.log("");
+        //         var movieInfo;
+        //         for (var i = 0; i < 5; i++) {
+        //             movieInfo = ("\n" + (response.data[0].lineup[0]) + "\n" +
+        //                 (response.data[i].venue.name) + "\n" +
+        //                 (response.data[i].venue.city) + "," +
+        //                 (response.data[i].venue.region) + "\n" +
+        //                 (moment(response.data[i].datetime).format("MM/DD/YYYY")) + "\n");
+        //             console.log(movieInfo);
+        //             fs.appendFile("concert-log.txt", movieInfo, function read(err, data) {
+        //                 if (err) {
+        //                     throw err;
+        //                 }
+        //             });
+        //         }
+        //     })
+        //     .catch(function (error) {
+        //         if (error.response) {
+        //             console.log(error.response.data);
+        //             console.log(error.response.status);
+        //             console.log(error.response.headers);
+        //         } else if (error.request) {
+        //             console.log(error.request);
+        //         } else {
+        //             console.log("Error", error.message);
+        //         }
+        //         console.log(error.config);
+        //     });
+
+    
+
+
+   
